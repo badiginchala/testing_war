1329b48b3aa1e4b218f7d0fdfe665b66ddaa1cf0
Dh2h7k8 <dhoot_anshul@gap.com>
2017-03-01 15:58:15 +0530

    base workspace for recruit service

---
+++ src/main/java/com/recruit/RecruitServiceApplication.java
@@ -0,0 +1,12 @@
+package com.recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitServiceApplication.class, args);
+	}
+}
