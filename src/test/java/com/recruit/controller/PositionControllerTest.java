3ce29df3f68f2ba84864bc1d246175e4052026ec
Deepanker Mishra <dmishra@osius.com>
2017-03-03 17:39:47 +0530

    Added tests for position controller and service

--- src/test/java/com/recruit/controller/PositionControllerTest.java
+++ src/test/java/com/recruit/controller/PositionControllerTest.java
@@ -1,9 +1,37 @@
 package com.recruit.controller;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import com.recruit.pojo.Position;
+import com.recruit.service.PositionService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PositionControllerTest {
+	
+	@InjectMocks
+	private PositionController positionController;
+	
+	@Mock
+	private PositionService positionService;
+	
+	@Test
+	public void testCreatePosition() {
+		positionController.createPosition(new Position());
+		verify(positionService, times(1)).preparePosition(any(Position.class));
+	}
+	
+	@Test
+	public void testGetPosition() {
+		positionController.getPosition();
+		verify(positionService, times(1)).getPosition();
+	}
 
 }
--- src/test/java/com/recruit/service/PositionServiceTest.java
+++ src/test/java/com/recruit/service/PositionServiceTest.java
@@ -1,5 +1,37 @@
 package com.recruit.service;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.recruit.pojo.Position;
+import com.recruit.repository.PositionRepository;
+
+@RunWith(MockitoJUnitRunner.class)
 public class PositionServiceTest {
+	
+	@InjectMocks
+	private PositionService positionService;
+	
+	@Mock
+	private PositionRepository positionRepository;
+	
+	@Test
+	public void testPreparePosition() {
+		positionService.preparePosition(new Position());
+		verify(positionRepository,times(1)).save(any(Position.class));
+	}
+	
+	@Test
+	public void testGetPosition() {
+		positionService.getPosition();
+		verify(positionRepository,times(1)).findAll();
+	}
 
 }
